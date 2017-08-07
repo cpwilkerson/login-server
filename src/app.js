4f2548bd276378d021996b1f8d13ba886be268f4
Christian Wilkerson <cwilkerson@ibaset.com>
2017-08-06 17:16:28 -0700

    Add eslint

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component {
+class App extends React.Component { // eslint-disable-line no-unused-vars
   render () {
     return (
       <div className="container-fluid">
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import express from 'express'
 var app = express()
 
 app.use((req, res, next) => {
-  console.log('LOG', req.path)
+  console.log('LOG', req.path) // eslint-disable-line
   next()
 })
 
 app.use(express.static('public'))
 
-console.log('Listening on port 3000')
+console.log('Listening on port 3000') // eslint-disable-line
 app.listen(3000)
