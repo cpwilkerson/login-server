761e52a877545d1b03b1f44a201f7f3395b7a18b
Christian Wilkerson <cwilkerson@ibaset.com>
2017-08-12 22:38:48 -0700

    Update testing setup

---
+++ test/config/node.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import sinon from 'sinon'
+// import sinonPromise from 'sinon-as-promised'
+import supertest from 'supertest'
+import sinonChai from 'sinon-chai'
+
+chai.use(chaiAsPromised)
+chai.use(sinonChai)
+
+global.sinon = sinon
+global.chai = chai
+global.supertest = supertest
+global.assert = chai.assert
+global.expect = chai.expect
+global.should = chai.should()
