3f955e43acf50a8d7f4e427f480f060125f5549c
Christian Wilkerson <cwilkerson@ibaset.com>
2017-08-12 22:50:50 -0700

    Change label for to htmlFor

--- src/login-view.js
+++ src/login-view.js
@@ -5,7 +5,7 @@ const LoginView = () => {
     <div>
       <form>
         <div className="form-group">
-          <label for="exampleInputEmail1">Email address</label>
+          <label htmlFor="exampleInputEmail1">Email address</label>
           <input type="email"
                  className="form-control"
                  id="exampleInputEmail1"
@@ -16,7 +16,7 @@ const LoginView = () => {
           </small>
         </div>
         <div className="form-group">
-          <label for="exampleInputPassword1">Password</label>
+          <label htmlFor="exampleInputPassword1">Password</label>
           <input type="password"
                  className="form-control"
                  id="exampleInputPassword1"
